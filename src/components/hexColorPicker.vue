65fdad8c4175dc16285ab573a32d73ded89b9350
Adam <aon2@cornell.edu>
2020-07-08 10:22:23 -0400

    Created conversions.json file and template

--- src/components/hexColorPicker.vue
+++ src/components/hexColorPicker.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="hexColorPicker">
     <h2>Color Picker Loaded</h2>
+    <input type="color" name="colorPicker" id="colorPicker" />
   </div>
 </template>
 
--- src/components/unitConverter.vue
+++ src/components/unitConverter.vue
@@ -1,12 +1,52 @@
 <template>
   <div id="unitConverter">
     <h2>Unit Converter Loaded</h2>
+    <select v-model="currentUnitType">
+      <option v-for="(type,index) in unitTypes " :key="index">{{ type }}</option>
+    </select>
+    <input type="text" />
+    <select v-model="unit1"></select>
+    <Label>=</Label>
+    <input type="text" />
+    <select v-model="unit2"></select>
+    <button>Convert</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "unitConverter"
+  name: "unitConverter",
+  data() {
+    return {
+      unitTypes: {
+        Length: [
+          "angstrom",
+          "um",
+          "mm",
+          "cm",
+          "m",
+          "km",
+          "lightyears",
+          "in",
+          "feet",
+          "yard",
+          "mile",
+          "nautical mile",
+          "parsec",
+          "au"
+        ],
+        Area: [],
+        Volume: [],
+        Weight: [],
+        Speed: [],
+        Energy: [],
+        Power: [],
+        Time: [],
+        Temperature: []
+      },
+      currentUnitType: "Length"
+    };
+  }
 };
 </script>
 
