246f084ca486593e4aafada6f21c9a902169c53f
Adam <aon2@cornell.edu>
2020-07-14 13:57:44 -0400

    Added HTML for Simple Interest calculator

--- src/components/tools/interestCalculator.vue
+++ src/components/tools/interestCalculator.vue
@@ -1,16 +1,57 @@
 <template>
   <div id="interestCalculator">
-    <h2>Interest Calculator Loaded</h2>
+    <div id="simpleInterest">
+      <h2>Simple Interest Calculator</h2>
+      <div class="interestSection">
+        <h4>Principal Amount:</h4>
+        <input type="number" id="simplePrincipal" />
+      </div>
+      <div class="interestSection">
+        <h4>Interest Rate:</h4>
+        <input type="number" id="simpleRate" />
+      </div>
+      <div class="interestSection">
+        <h4>Period Length</h4>
+        <select v-model="simpleFrequency">
+          <option v-for="freq in frequencies" :key="freq">{{ freq }}</option>
+        </select>
+      </div>
+      <div class="interestSection">
+        <h4>Number of Periods</h4>
+        <input type="number" id="simplePeriods" />
+      </div>
+      <button>Calculate</button>
+      <hr>
+    </div>
+    <div id="compoundInterest">
+      <h2>Compound Interest Calculator</h2>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "interestCalculator"
+  name: "interestCalculator",
+  data() {
+    return {
+      frequencies: [
+        "annually",
+        "monthly",
+        "quarterly",
+        "weekly",
+        "bi-monthly",
+        "daily"
+      ],
+      simpleFrequency: "annually"
+    };
+  }
 };
 </script>
 <style scoped>
 #interestCalculator {
   border: solid orangered;
+}
+h4 {
+  display: inline;
 }
 </style>
