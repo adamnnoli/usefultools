0f74652cce50a1c0b5d4e602675422d976000214
Adam <aon2@cornell.edu>
2020-07-15 10:16:09 -0400

    Added HTML for all Interest Calculators

--- src/components/tools/interestCalculator.vue
+++ src/components/tools/interestCalculator.vue
@@ -4,27 +4,73 @@
       <h2>Simple Interest Calculator</h2>
       <div class="interestSection">
         <h4>Principal Amount:</h4>
-        <input type="number" id="simplePrincipal" />
+        <input type="number" />
       </div>
       <div class="interestSection">
-        <h4>Interest Rate:</h4>
-        <input type="number" id="simpleRate" />
-      </div>
-      <div class="interestSection">
-        <h4>Period Length</h4>
-        <select v-model="simpleFrequency">
-          <option v-for="freq in frequencies" :key="freq">{{ freq }}</option>
-        </select>
+        <h4>Interest Rate per Period:</h4>
+        <input type="number" />
       </div>
       <div class="interestSection">
         <h4>Number of Periods</h4>
-        <input type="number" id="simplePeriods" />
+        <input type="number" />
       </div>
       <button>Calculate</button>
-      <hr>
+      <hr />
     </div>
     <div id="compoundInterest">
       <h2>Compound Interest Calculator</h2>
+      <div class="compoundSection">
+        <h4>Principal Amount</h4>
+        <input type="number" />
+      </div>
+      <div class="compoundSection">
+        <h4>Addition each Period</h4>
+        <input type="number" />
+      </div>
+      <div class="compoundSection">
+        <h4>Number of Periods</h4>
+        <input type="number" />
+      </div>
+      <div class="compoundSection">
+        <h4>Interest Rate</h4>
+        <input type="number" />
+      </div>
+      <hr />
+    </div>
+    <div id="presentValue">
+      <h2>Present Value Calculator</h2>
+      <div class="presentValueSection">
+        <h4>Future Value</h4>
+        <input type="number" />
+      </div>
+      <div class="presentValueSection">
+        <h4>Number of Periods</h4>
+        <input type="number" />
+      </div>
+      <div class="presentValueSection">
+        <h4>Compounding Times per Period</h4>
+        <input type="number" />
+      </div>
+      <div class="presentValueSection">
+        <h4>Interest Rate per Period</h4>
+        <input type="number" />
+      </div>
+      <hr />
+    </div>
+    <div id="annuity">
+      <h2>Present Value of an Annuity Calculator</h2>
+      <div class="annuitySection">
+        <h4>Payment Amount</h4>
+        <input type="number" />
+      </div>
+      <div class="annuitySection">
+        <h4>Interest Rate</h4>
+        <input type="number" />
+      </div>
+      <div class="annuitySection">
+        <h4>Number of Periods</h4>
+        <input type="number" />
+      </div>
     </div>
   </div>
 </template>
